819f1409d094e22b5e21d43e2c27aee79903d22a
Yurii Timofeev <georgius@ural.ru>
2024-07-08 10:09:10 +0500

    Added articles

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,6 +2,9 @@
   <nav class="navbar navbar-expand-lg bg-primary mb-4">
     <div class="container-fluid">
       <NuxtLink to="/" class="navbar-brand text-light">Home</NuxtLink>
+      <NuxtLink to="/articles" class="text-white">
+        Articles
+      </NuxtLink>
       <div class="flex-grow"></div>
       <button v-if="!size" data-testid="checkout" class="btn btn-secondary">Your cart is empty</button>
       <NuxtLink to="/checkout" v-else data-testid="checkout" class="btn btn-secondary">
---
+++ pages/articles/[[slug]].vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container articles py-5">
+    <ContentDoc :path="`/${path}`" />
+    <hr>
+    <NuxtLink v-if="path !== 'list'" to="/articles">Back to list of articles</NuxtLink>
+  </div>
+</template>
+<script setup>
+  const route = useRoute();
+  const path = route.params.slug || "list";
+</script>
